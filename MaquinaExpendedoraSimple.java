d0539e3c5715656e5da6e27a14972becd8e9f2cd
Patri <p.santamartamedina@gmail.com>
2018-10-26 12:31:54 +0200

    Ahora el billete es de León a Madrid a 12 euros

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -1,4 +1,4 @@
-class MaquinaExpendedoraSimple {
+public class MaquinaExpendedoraSimple {
     
     // El precio del billete
     private int precioBillete;
@@ -16,12 +16,12 @@ class MaquinaExpendedoraSimple {
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple(int costeBillete, String origen, String destino) {
-        precioBillete = costeBillete;
+    public MaquinaExpendedoraSimple() {
+        precioBillete = 12;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
-        estacionOrigen = origen;
-        estacionDestino = destino;
+        estacionOrigen = "León";
+        estacionDestino = "Madrid";
     }
 
     /**
